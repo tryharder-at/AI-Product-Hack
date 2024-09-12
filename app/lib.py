dea72e8227c3197fc8ad14dd643a89651e8b5c11
Rozanov Konstantin <159526917+KissedByF1re@users.noreply.github.com>
2024-09-12 13:07:51 +0300

    Функции для подсчета средних значений по магазину и группе

--- app/lib.py
+++ app/lib.py
@@ -26,8 +26,31 @@ def summ_sales_data(df, date_column, granularity):
         grouped_df = df.groupby(pd.Grouper(key=date_column, freq='M'))[columns_to_sum].sum().reset_index()
     else:
         raise ValueError("Неверно задана гранулярность. Используйте 'Day', 'Week' или 'Month'.")
+            
     return grouped_df
 
+def preprocess_data_for_prediction(df):
+    df['store_number'] = df['store_id'].str.extract(r'(\d+)').astype(int)
+    df['group_id'] = df['item_id'].str.extract(r'STORE_\d_(\d)').astype(int)
+        
+    return df
+
+def calculate_group_means(df):
+    group_means = df.groupby(['store_number', 'group_id']).agg(
+        cnt=('cnt', 'mean'),
+        sell_price=('sell_price', 'mean'),
+        cashback_1=('CASHBACK_STORE_1', 'sum'),
+        cashback_2=('CASHBACK_STORE_2', 'sum'),
+        cashback_3=('CASHBACK_STORE_3', 'sum')
+    ).reset_index()
+    
+    group_means['cashback_2'] = group_means.apply(
+        lambda row: 0 if row['cashback_1'] > 0 else row['cashback_2'], axis=1)
+    group_means['cashback_3'] = group_means.apply(
+        lambda row: 0 if row['cashback_1'] > 0 else row['cashback_3'], axis=1)
+        
+    return group_means
+        
 def line_plot_with_legend(df: pd.DataFrame, variables: list[str]) -> plt.Figure:
     # для теста))
     df = df[df['item_id'] == 'STORE_2_085']
